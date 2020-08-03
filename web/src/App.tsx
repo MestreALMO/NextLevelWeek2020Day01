7b4d099bbcc6db1afeff32bb76125eadba81c00c
André Lusegardis <lusegardis@gmail.com>
2020-08-03 20:54:46 -0300

    01:32:34
    proximo passo:
    yarn add @types/react-router-dom -D

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Landing from './pages/Landing'; 
+/* automaticamente vai para o arquivo index, se colocar apenas o nome da página */
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
