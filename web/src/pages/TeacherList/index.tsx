4c192d517d1c8d14c70807add7c28a54cc2a7c33
André Lusegardis <lusegardis@gmail.com>
2020-08-03 22:54:48 -0300

    1:57:30 - video time

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
 function TeacherList() {
     return(
-        <h1>Teacher List</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis." />
+        </div>
     )
 }
 
